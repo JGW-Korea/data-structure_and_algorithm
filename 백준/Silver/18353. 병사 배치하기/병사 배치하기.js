3f464779007e89dab464948af96af24e9bf59351
JGW-Korea <94373324+JGW-Korea@users.noreply.github.com>
2025-02-07 16:53:48 +0900

    [Silver II] Title: 병사 배치하기, Time: 180 ms, Memory: 10400 KB -BaekjoonHub

---
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.js
@@ -0,0 +1,36 @@
+function lowerBound(arr, target, start, end) {
+
+  while (start < end) {
+
+    let mid = parseInt((start + end) / 2);
+    if(arr[mid] >= target) end = mid;
+    else start = mid + 1;
+    
+  }
+
+  return end;
+  
+}
+
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let n = Number(input[0]);
+let arr = input[1].split(' ').map(Number);
+
+arr.reverse()
+
+let d = [0];
+
+for(let x of arr) {
+
+  if(d[d.length-1] < x) {
+    d.push(x);
+  } else {
+
+    let index = lowerBound(d, x, 0, d.length);
+    d[index] = x;
+    
+  }
+}
+
+console.log(n - (d.length - 1));
