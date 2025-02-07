961db772952db83c95d458e02cd38ece836664dc
JGW-Korea <94373324+JGW-Korea@users.noreply.github.com>
2025-02-07 16:23:31 +0900

    [Bronze V] Title: A+B, Time: 108 ms, Memory: 12848 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 입력 도구 : Scanner, BufferdReader
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println(sc.nextInt() + sc.nextInt());
+		
+		sc.close();
+	}
+
+}
