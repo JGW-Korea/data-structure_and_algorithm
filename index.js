200194aa32dcaa0cbf94786c6bb030f8e16d8684
JGW-Korea <jgw6372@gmail.com>
2025-02-10 22:12:04 +0900

    docs: 크루스칼 알고리즘 내용 정리 완료

--- index.js
+++ index.js
@@ -1,106 +1,46 @@
-// 우선순위 큐 알고리즘 정의
-class MinHeap {
-  constructor() {
-    this.heap = [null];
+// 파인드(find) 연산: 특정 정점의 대표 정점을 찾기 위한 연산
+function find(parent, x) {
+  if (parent[x] === x) {
+    return x;
   }
 
-  insert(value) {
-    this.heap.push(value); // 가장 마지막 위치에 원소를 삽입한다.
+  // 경로 압축 최적화: 대표 정점을 찾기 위한 경로에 있는 모든 원소들이 직접 최상위 부모를 가리키도록 변경
+  return (parent[x] = find(parent, parent[x]));
+}
+// 유니온(Union) 연산: 두 개의 서로서 집합을 하나의 집합으로 병합
+function union(parent, a, b) {
+  a = find(parent, a);
+  b = find(parent, b);
 
-    // 이진 트리는 계산을 통해 부모 정점의 위치를 알 수 있다.
-    let currentIndex = this.heap.length - 1;
-    let parentIndex = Math.floor(currentIndex / 2);
-
-    // heapify-up 로직
-    while (
-      parentIndex < this.heap.length &&
-      this.heap[currentIndex] < this.heap[parentIndex]
-    ) {
-      [this.heap[currentIndex], this.heap[parentIndex]] = [
-        this.heap[parentIndex],
-        this.heap[currentIndex],
-      ];
-
-      currentIndex = parentIndex;
-      parentIndex = Math.floor(currentIndex / 2);
-    }
-  }
-
-  remove() {
-    if (this.heap.length - 1 === 0) return; // 예외 처리 -> 이진 트링에 정점이 없을 경우 아무것도 반환하지 않고 메서드 종료
-    if (this.heap.length - 1 === 1) return this.heap.pop(); // 이진 트리에 정점이 하나밖에 없을 경우
-
-    const returnValue = this.heap[1]; // 반환 값으로 루트 노드의 정보를 가져온다.
-    this.heap[1] = this.heap.pop(); // 루트 노드에 가장 마지막 높이의 리프 노드를 배치시킨다. (임시적)
-
-    let currentIndex = 1;
-    let leftIndex = currentIndex * 2;
-    let rightIndex = currentIndex * 2 + 1;
-
-    // heapify-down 로직
-    while (
-      (leftIndex < this.heap.length &&
-        this.heap[leftIndex] < this.heap[currentIndex]) ||
-      (rightIndex < this.heap.length &&
-        this.heap[rightIndex] < this.heap[currentIndex])
-    ) {
-      // 왼쪽 자식 노드의 값이 오른쪽 자식 노드의 값보다 작을 경우
-      if (this.heap[leftIndex] < this.heap[rightIndex]) {
-        [this.heap[currentIndex], this.heap[leftIndex]] = [
-          this.heap[leftIndex],
-          this.heap[currentIndex],
-        ];
-        currentIndex = leftIndex;
-      }
-
-      // 오른쪽 자식 노드의 값이 왼쪽 자식 노드의 값보다 작을 경우
-      else {
-        [this.heap[currentIndex], this.heap[rightIndex]] = [
-          this.heap[rightIndex],
-          this.heap[currentIndex],
-        ];
-        currentIndex = rightIndex;
-      }
-
-      leftIndex = currentIndex * 2;
-      rightIndex = currentIndex * 2 + 1;
-    }
-
-    return returnValue; // 루트 노드의 정보를 반환한다.
-  }
-
-  isEmpty() {
-    return this.heap.length - 1 === 0;
+  // 더 작은 값을 기준으로 서로서 집합을 하나의 집합으로 병합한다.
+  if (a < b) {
+    parent[b] = a;
+  } else {
+    parent[a] = b;
   }
 }
 
-function solution(node, startNode, graph) {
-  const dist = new Array(node + 1).fill(Infinity);
-  dist[startNode] = 0;
+// 두 집합이 동일한 대표 루트를 가리키고 있는지 확인
+function compare(parent, a, b) {
+  a = find(parent, a);
+  b = find(parent, b);
 
-  const pq = new MinHeap();
-  pq.insert({ node: startNode, cost: 0 });
+  // 같은 집합일 경우 사이클이 발생할 수 있음
+  return a === b;
+}
 
-  // 다익스트라 알고리즘
-  while (!pq.isEmpty()) {
-    const { node: currentNode, cost: currentCost } = pq.remove(); // 가장 간선의 합이 낮은 정점과 간선의 합을 가지고 온다.
+function solution(V, graph) {
+  let parent = Array.from({ length: V + 1 }, (_, idx) => idx); // 유니온 연산에 필요한 1차원 배열 정의 + 초기화는 자기 자신을 부모로 지정
+  let answer = 0;
 
-    for (const [nextNode, nextDist] of graph[currentNode]) {
-      const nextCost = currentCost + nextDist; // 현재 거리와 다음 정점까지의 거리를 계산한다.
+  // 그래프의 간선을 가중치 값을 기준으로 정렬
+  graph.sort((a, b) => a[2] - b[2]);
 
-      if (nextCost < dist[nextNode]) {
-        dist[nextNode] = nextCost;
-        pq.insert({ node: nextNode, cost: nextCost });
-      }
+  for (const [a, b, cost] of graph) {
+    if (!compare(parent, a, b)) {
+      answer += cost;
+      union(parent, a, b);
     }
-  }
-
-  // 결과 출력
-  let answer = "";
-
-  for (let i = 1; i <= node; i++) {
-    if (dist[i] !== Infinity) answer += dist[i] + "\n";
-    else answer += "INF";
   }
 
   return answer;
@@ -110,12 +50,10 @@ const fs = require("fs");
 const input = fs.readFileSync("index.txt").toString().trim().split("\n");
 
 const [V, E] = input[0].split(" ").map(Number);
-const K = input[1].split(" ").map(Number);
-const graph = Array.from({ length: V + 1 }, () => []);
+const graph = [];
 
-for (let i = 2; i < E + 2; i++) {
-  const [from, to, cost] = input[i].split(" ").map(Number);
-  graph[from].push([to, cost]);
+for (let i = 1; i < E + 1; i++) {
+  graph.push(input[i].split(" ").map(Number));
 }
 
-console.log(solution(V, K, graph));
+console.log(solution(V, graph));
