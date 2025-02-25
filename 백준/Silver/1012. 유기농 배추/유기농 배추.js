cce1352e9acf7f904443ab269301514cd9f973fa
JGW-Korea <94373324+JGW-Korea@users.noreply.github.com>
2025-02-25 15:22:41 +0900

    [Silver II] Title: 유기농 배추, Time: 184 ms, Memory: 12776 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.js
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.js
@@ -1,54 +1,64 @@
-function solution(m, n, graph) {
-  // 배추 영역을 위한 방문 처리
-  const visited = Array.from({length: n}, () => new Array(m).fill(0));
-  let worms = 0; // 최소 지렁이 마리 수
+function solution(M, N, graph) {
+  // 방문 처리를 위한 배열 NxM 크기의 이차원 배열을 생성한다.
+  const visited = Array.from({ length: N }, () => new Array(M).fill(0));
+  const queue = []; // 시작 위치가 명시되어 있지 않기 때문에 큐를 생성만 한다.
 
-  function dfs(x, y) {
-    const dx = [1, 0, -1, 0]; // 열 이동 가능 경로
-    const dy = [0, -1, 0, 1]; // 행 이동 가능 경로
+  const dx = [-1, 0, 1, 0];
+  const dy = [0, 1, 0, -1];
 
-    // 현재 좌표에 방문처리를 해준다.
-    visited[x][y] = 1;
+  let count = 0; // 흰색 지렁이 개수
 
-    for(let i = 0; i < 4; i++) {
-      const [nextX, nextY] = [x + dx[i], y + dy[i]];
+  // 시작 위치를 찾기 위해 2중 반복문을 수행한다.
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < M; j++) {
+      // 배추가 심어진 곳을 확인 후 해당 위치를 방문하지 않았을 경우
+      if (graph[i][j] && !visited[i][j]) {
+        visited[i][j] = 1; // 해당 위치를 방문처리를 한다.
+        queue.push([i, j]); // BFS 탐색을 위해 큐에 시작 위치를 추가한다.
 
-      // 이동 가능한 좌표가 배열 범위를 벗어나면 생략한다.
-      if(nextX < 0 || nextY < 0 || nextX >= n || nextY >= m) continue;
-      if(graph[nextX][nextY] === 1 && !visited[nextX][nextY]) {
-        dfs(nextX, nextY);
+        count += 1; // 흰색 지렁이를 한 마리 추가한다.
+
+        while (queue.length) {
+          const [currentX, currentY] = queue.shift();
+
+          for (let move = 0; move < 4; move++) {
+            const [nextX, nextY] = [currentX + dx[move], currentY + dy[move]];
+
+            if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) continue;
+            if (graph[nextX][nextY] && !visited[nextX][nextY]) {
+              visited[nextX][nextY] = 1;
+              queue.push([nextX, nextY]);
+            }
+          }
+        }
       }
     }
   }
-  
-  for(let i = 0; i < n; i++) {
-    for(let j = 0; j < m; j++) {
-      if(graph[i][j] === 1 && !visited[i][j]) { // 배추 영역이자, 방문하지 않은 곳을 찾는다.
-        dfs(i, j); // dfs를 통해 모든 배추 영역을 방문 시킨다.
-        worms += 1; // dfs에서 탈출하면, 지렁이를 1마리 증가시킨다.
-      }
-    }
-  }
-  
-  return worms;
+
+  return count;
 }
 
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const tc = Number(input.shift());
-let result = '';
+let tc = Number(input[0]);
+let answer = "";
+let idx = 1;
 
-for(let i = 1; i <= tc; i++) {
-  const [m, n, k] = input.shift().split(' ').map(Number);
-  const graph = Array.from({length: n}, () => new Array(m).fill(0));
+// 각 Test Case의 흰색지렁이 최소 개수를 확인한다.
+for (let i = 0; i < tc; i++) {
+  const [M, N, K] = input[idx++].split(" ").map(Number); // 밭의 크기와 배추가 심어진 위치의 개수를 가져온다.
 
-  for(let j = 0; j < k; j++) {
-    const [x, y] = input.shift().split(' ');
+  // 밭의 크기를 지정한다.
+  const graph = Array.from({ length: N }, () => new Array(M).fill(0));
+
+  // 배추가 심어진 위치를 1로 표시한다.
+  for (let pos = 0; pos < K; pos++) {
+    const [x, y] = input[idx++].split(" ").map(Number);
     graph[y][x] = 1;
   }
 
-  result += solution(m, n, graph) + '\n';
+  answer += solution(M, N, graph) + "\n";
 }
 
-console.log(result);
+console.log(answer);
