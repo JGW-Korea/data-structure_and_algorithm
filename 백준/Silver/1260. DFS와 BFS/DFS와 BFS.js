c8e63ea9f702bee2d20092c78c3c17704643627a
조계원 <jgw6372@gmail.com>
2025-03-07 11:52:34 +0900

    [Silver II] Title: DFS와 BFS, Time: 184 ms, Memory: 15872 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.js
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.js
@@ -1,73 +1,112 @@
-// DFS 알고리즘
-function dfs(graph, node, start) {
-  const visited = new Array(node + 1).fill(0); // 방문 처리를 위한 배열
-  const result = [];
+function solution(N, M, V, graph) {
+  // 2개의 과정을 해줘야돼 (DFS 순서), (BFS 순서)
 
-  // 렉시컬 스코프 성질을 이용하여 중첩 함수를 선언하여 dfsWorks에 있는 변수는 상위 스코프의 변수를 참조할 수 있게 함
-  function dfsWorks(currentNode) {
-    visited[currentNode] = 1; // 현재 정점 방문 처리
-    result.push(currentNode); // 방문한 정점 결과에 포함
+  let dfsSearch = "";
+  let bfsSearch = "";
 
-    // 이동 가능한 정점 순회
-    for (const nextNode of graph[currentNode]) {
-      if (!visited[nextNode]) {
-        dfsWorks(nextNode);
+  // DFS
+  function dfs() {
+    const visited = new Array(N + 1).fill(0); // Node 개수만큼의 크기를 가진 방문 배열을 생성한다.
+
+    // DFS -> 스택 또는 재귀
+    // 스택 -> 삽입, 삭제 -> O(1)
+    // 재귀 -> O(?) -> 성능 제한 + 이차 시간(하나의 함수에 두 개이상의 자기 자신을 호출)
+
+    // 재귀를 할 때 가장 중요한거는 -> 매개변수를 어떻게 사용할 것인가
+    // current => V
+    // 다시 자기 자신을 재호출(재귀 호출) -> 현재(current) 정점에서 이동 가능한 정점을 보내겠다
+
+    /*
+      1 - 2 - 4
+        - 3 - 4
+        - 4
+    */
+
+    function recursion(current) {
+      visited[current] = 1; // 방문 처리를 할것입니다.
+      dfsSearch += current + " ";
+
+      // 현재 정점에서 연결된 간선을 이동을 해야된다.
+      for (const next of graph[current]) {
+        if (!visited[next]) {
+          recursion(next);
+        }
+      }
+    }
+
+    // V -> 시작 정점이라고 입력에서 주어졌기 때문이야
+    // 시작 정점부터 깊이 탐색을 해야되는거고
+    // V를 보내주게 된거고
+    recursion(V);
+  }
+
+  // BFS
+  function bfs() {
+    const visited = new Array(N + 1).fill(0); // Node 개수만큼의 크기를 가진 방문 배열을 생성한다. (방문 처리를 위함)
+    const queue = [V]; // 시작 노드를 바로 담는 큐를 초기화 시킨다.
+
+    visited[V] = 1;
+    // bfsSearch += V;
+
+    while (queue.length) {
+      const current = queue.shift();
+      bfsSearch += current + " ";
+
+      // 현재 정점에서 연결된 간선을 이동을 해야된다.
+      for (const next of graph[current]) {
+        if (!visited[next]) {
+          // 방문하지 않았는지 체크
+          queue.push(next);
+          visited[next] = 1;
+        }
       }
     }
   }
 
-  // dfs 탐색을 수행
-  dfsWorks(start);
+  dfs();
+  bfs();
 
-  return result.join(" ");
+  return dfsSearch + "\n" + bfsSearch;
 }
 
-// BFS 알고리즘
-function bfs(graph, node, start) {
-  const visited = new Array(node + 1).fill(0); // 방문 처리를 위한 배열
-  visited[start] = 1; // 시작 정점 방문 처리
-
-  const queue = [start]; // BFS 알고리즘을 위한 큐 자료구조
-  const result = [];
-
-  // BFS 탐색 (큐에 정점이 없을 때까지 순회)
-  while (queue.length) {
-    const currentNode = queue.shift(); // 현재 정점을 가져온다.
-    result.push(currentNode); // 결과에 포함
-
-    // 이동 가능한 정점 순회
-    for (const nextNode of graph[currentNode]) {
-      if (!visited[nextNode]) {
-        visited[nextNode] = 1; // 방문 처리
-        queue.push(nextNode); // 이동 가능한 정점 큐 자료구조에 삽입
-      }
-    }
-  }
-
-  return result.join(" ");
-}
-
+// BFS, DFS read, write가 다 될 수 있는 문제가 -> 백준 DFS와 BFS 문제입니다.
 const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-// input.shift()를 이용해서 하면 그래프를 표현을 forEach() 메서드를 통해서 할 수 있음
-// 다만, 간선의 개수(M)이 최대 10,000이기 때문에 배열 자료구조 관념에서 보면 앞으로 당겨오는 작업이 포함되어 있기 때문에
-// 성능상 안좋을 수 있음
+// "4 5 1"
 const [N, M, V] = input[0].split(" ").map(Number);
-
-// 인접 리스트 방식으로 그래프 표현 (Node 번호는 1부터 시작)
 const graph = Array.from({ length: N + 1 }, () => []);
 
-for (let i = 1; i < M + 1; i++) {
-  const [from, to] = input[i].split(" ").map(Number); // 정점이 연결된 구간을 가져옴
+// 간선의 개수 M(1 ≤ M ≤ 10,000)
+/*
+  A -> B로 가는 간선
+  간서의 개수 M
+  1 2 (1) -> (2)
+  1 3 (1) -> (3)
+  1 4 (1) -> (4)
+  2 4 (2) -> (4)
+  3 4 (3) -> (4)
+*/
 
-  // 무방향 연결 리스트이기 때문에 간선을 하나로 취급
-  graph[from].push(to);
-  graph[to].push(from);
+/*
+  // 양방향은 무방향 그래프
+  // 결국에는 두 간선이 같은 간선으로 취급된다.
+  1 - 2 - 4
+    - 3 - 4
+    - 4
+
+*/
+
+for (let i = 1; i < M + 1; i++) {
+  const [from, to] = input[i].split(" ").map(Number);
+
+  graph[from].push(to); // A -> B로 갈 수 있다.
+  graph[to].push(from); // B -> A로 갈 수 있다.
 }
 
-// 정점이 여러 개일 경우 작은 정점 번호부터 이동을 해야되기 때문에 각 정점이 갈 수 있는 정점의 번호를 정렬을 시킨다.
-graph.forEach((node) => node.sort((a, b) => a - b));
+// 단, 방문할 수 있는 정점이 여러 개인 경우에는 정점 번호가 작은 것을 먼저 방문하고 (정렬)
+graph.forEach((node) => {
+  node.sort((a, b) => a - b);
+});
 
-console.log(dfs(graph, N, V)); // DFS 수행 결과
-console.log(bfs(graph, N, V)); // BFS 수행 결과
+console.log(solution(N, M, V, graph));
