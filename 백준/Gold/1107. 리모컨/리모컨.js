aa48cc3ba994a0db3ce8b24948ff5fdb9dcb0bb3
조계원 <jgw6372@gmail.com>
2025-03-20 19:57:00 +0900

    [Gold V] Title: 리모컨, Time: 460 ms, Memory: 28128 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.js
+++ 백준/Gold/1107. 리모컨/리모컨.js
@@ -1,22 +1,20 @@
 function solution(target, broken, brokenBtn) {
-  if (target === 100) return 0; // 시작 채널이 목표 채널이면 0
+  if (target === 100) return 0; // 이동하고 싶은 채널이 현재 채널일 경우
 
-  // 현재 누른 번호에 부서진 번호가 포함되었는지 여부 반환
-  const isBroken = (num) => {
+  function isBroken(num) {
     return num
       .toString()
       .split("")
-      .some((digit) => brokenBtn.includes(Number(digit)));
-  };
+      .some((btnNumber) => brokenBtn.includes(Number(btnNumber)));
+  }
 
-  let answer = Math.abs(target - 100); // 100번에서 +, - 버튼만으로 이동하는 경우
+  let answer = Math.abs(100 - target); // +, - 기호만 눌러서 도달한 값
 
-  // 0 ~ 999999 까지 모든 채널을 탐색 (최대 6자리 숫자)
-  for (let i = 0; i <= 999999; i++) {
-    if (!isBroken(i)) {
-      // 해당 번호를 숫자 버튼으로 누를 수 있는 경우
-      const count = i.toString().length + Math.abs(i - target);
-      answer = Math.min(answer, count);
+  // 0 ~ 999,999 까지 모든 번호에 대해서 최소값을 구함
+  for (let pressBtn = 0; pressBtn <= 999999; pressBtn++) {
+    if (!isBroken(pressBtn)) {
+      const press = pressBtn.toString().length + Math.abs(pressBtn - target);
+      answer = Math.min(answer, press);
     }
   }
 
@@ -28,6 +26,7 @@ const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const target = Number(input[0]);
 const broken = Number(input[1]);
+// 부서진 버튼이 있을 수도 있고 없을 수도 있음
 const brokenBtn = broken === 0 ? [] : input[2].split(" ").map(Number);
 
 console.log(solution(target, broken, brokenBtn));
