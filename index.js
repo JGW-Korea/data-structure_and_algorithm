eca1f92e4a160acea8232c5247c62d2b8ac00e3e
JGW-Korea <jgw6372@gmail.com>
2025-05-10 19:21:13 +0900

    docs: 정답 코드 추가

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
-function solution(n, k) {
-  const numbers = Array.from({ length: n }, (_, idx) => idx + 1); // 원을 두르고 앉아있는 사람들
+const white = ["WBWBWBWB", "BWBWBWBW", "WBWBWBWB", "BWBWBWBW", "WBWBWBWB", "BWBWBWBW", "WBWBWBWB", "BWBWBWBW"]; // 흰색 체스판
+const black = ["BWBWBWBW", "WBWBWBWB", "BWBWBWBW", "WBWBWBWB", "BWBWBWBW", "WBWBWBWB", "BWBWBWBW", "WBWBWBWB"]; // 검은색 체스판
 
-  const answer = [];
+function solution(m, n, board) {
+  let answer = Number.MAX_SAFE_INTEGER;
 
-  // 1 2 3 4 5 6 7 -> 3 (3)
-  // 1 2 4 5 6 7 -> 6 (6)
-  // 1 2 4 5 7 -> 2 (9)
-  // 1 4 5 7 -> 7 (12)
-  // 1 4 5 -> 5 (15)
-  // 1 4 -> 1 (18)
-  // 4 -> 4 (21)
-  while (answer.length !== n) {
-    let count = 1;
-    while (count !== k) {
-      numbers.push(numbers.shift());
-      count += 1;
+  function check(x, y) {
+    let whiteCount = 0; // 흰색 개수
+    let blackCount = 0; // 검은색 개수
+
+    // -> 주어진 체스판을 기존 흰색, 검은색 체스판 규칙에 맞게 바꿔야 될 위치를 계산한다.
+    for (let i = x; i < x + 8; i++) {
+      for (let j = y; j < y + 8; j++) {
+        if (board[i][j] !== white[i - x][j - y]) whiteCount += 1;
+        if (board[i][j] !== black[i - x][j - y]) blackCount += 1;
+      }
     }
 
-    answer.push(numbers.shift());
+    return Math.min(whiteCount, blackCount); // 가장 적게 바꾼 바둑판으로 교체한다.
   }
 
-  return `<${answer.join(", ")}>`;
+  // -> 주어진 체스판과 기존 흰색, 검은 체스판 차이까지만 증가시킨다.
+  for (let row = 0; row <= m - 8; row++) {
+    for (let col = 0; col <= n - 8; col++) {
+      answer = Math.min(answer, check(row, col));
+    }
+  }
+
+  return answer;
 }
 
 const fs = require("fs");
-const [N, K] = fs.readFileSync("index.txt").toString().trim().split(" ").map(Number);
+const input = fs.readFileSync("index.txt").toString().trim().split("\n");
 
-console.log(solution(N, K));
+const [M, N] = input[0].split(" ").map(Number);
+const board = input.slice(1).map((element) => element.split(""));
+
+console.log(solution(M, N, board));
