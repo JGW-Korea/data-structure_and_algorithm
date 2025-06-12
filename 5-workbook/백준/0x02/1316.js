df87976d9add39d90fedeb5ffdc43554a198b379
JGW-Korea <jgw6372@gmail.com>
2025-06-12 17:27:43 +0900

    docs: 1316번 문제 추가

--- 5-workbook/백준/0x02/1316.js
+++ 5-workbook/백준/0x02/1316.js
@@ -15,21 +15,26 @@ function solution(n, strSet) {
 
     for (let i = 1; i < str.length; i++) {
       const next = str[i];
-      
-      if (current === next) continue; // 현재 단어와 다음 단어가 동일한 경우 넘어감
-      else if(current !== next && !set.has(next)) {
+
+      // 현재 단어와 다음 단어가 동일한 경우 넘어감
+      if (current === next) continue;
+      // 현재 단어와 다음 단어가 동일하지 않은 경우
+      else if (current !== next && !set.has(next)) {
         current = next;
         set.add(next);
-      } else {
+      }
+
+      // 모든 조건이 맞지 않을 경우
+      else {
         flag = true;
-        break
+        break;
       }
     }
 
-    if(!flag) answer += 1;
+    if (!flag) answer += 1;
   }
 
-  return answer
+  return answer;
 }
 
 console.log(solution(N, strSet));
