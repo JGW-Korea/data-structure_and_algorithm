f1caa5caf8d84fa0a68b0429af4519d1a0925844
JGW-Korea <jgw6372@gmail.com>
2025-06-13 12:25:59 +0900

    docs: 붕대감기, 석유시추 문제 코드 수정

--- 5-workbook/프로그래머스/lv1/붕대감기.js
+++ 5-workbook/프로그래머스/lv1/붕대감기.js
@@ -1,44 +1,36 @@
 // 1. t초마다 체력을 회복 (1초: x만큼, 연속: y만큼), 단, 최대 체력 이상은 불가능
-
-// 기술을 쓰는 도중 몬스터에게 공격을 당하면 기술이 취소되고, 체력을 회복할 수 없다.
-// 몬스터에게 공격당해 기술이 취소당하거나 기술이 끝나면 그 즉시 붕대 감기를 다시 사용하며,
-// 연속 성공 시간이 0으로 초기화된다.
-
-// 3. 공격을 받으면 피해량만큼 체력이 줄고, 0이하가 되면 죽음
-
+// 2. 피격 당할 시 체력이 깍이고, 회복 시전 시간이 0초로 초기화된다. 단, 피격 시 체력이 0이하일 경우 캐릭터가 사망
 // -> 캐릭터가 끝까지 생존할 수 있는지에 대한 여부를 반환(남은 체력 | -1)
-
 function solution(bandage, health, attacks) {
-  const [castingTime, second, plus] = bandage; // [시전 시간, 초당 회복, 추가 회복]
+  const [castingTime, second, plus] = bandage; // [시전시간, 초당, 추가]
+  const maxTime = attacks.at(-1)[0]; // 게임 최대 시간
 
-  let answer = health; // 남은 체력
-  let successTime = 0; // 붕대 감기 성공 시간
+  let answer = health; // 초기 체력값은 최대 체력으로 초기화
 
-  const maxTime = attacks.at(-1)[0]; // 최대 시간
-  let attackIdx = 0; // 공격 순서
+  let attackIdx = 0; // 공격 정보 순서(index)
+  let successTime = 0; // 회복 성공 시간
 
   for (let time = 1; time < maxTime + 1; time++) {
-    // 1. 공격 시점일 경우
-    if (attacks[attackIdx]?.[0] === time) {
-      // 현재 체력 감속 및 회복 연속 시간 초기화
-      answer -= attacks[attackIdx][1];
+    // 1. 몬스터가 공격하는 시점
+    if (time === attacks[attackIdx]?.[0]) {
+      answer -= attacks[attackIdx++][1];
       successTime = 0;
 
-      attackIdx += 1;
-      if (answer <= 0) return -1; // 체력이 0이하일 경우
+      if (answer <= 0) return -1;
     }
 
-    // 2. 공격 시점이 아닐 경우 -> 회복 가능
+    // 2. 몬스터가 공격하지 않는 시점
     else {
       successTime += 1;
 
-      if (successTime !== castingTime) answer += second; // 연속 회복 시간이 동일하지 않은 경우
+      if (successTime !== castingTime) answer += second; // 초당 회복
       else {
+        // 추가 회복
         answer += second + plus;
         successTime = 0;
       }
 
-      // 최대 체력보다 넘어갈 경우 초기화
+      // 회복된 체력이 최대치를 넘어선 경우
       if (answer > health) answer = health;
     }
   }
--- 5-workbook/프로그래머스/lv2/석유시추.js
+++ 5-workbook/프로그래머스/lv2/석유시추.js
@@ -1,45 +1,46 @@
+// 1. N x M 크기의 땅 속에서 석유가 여러 덩어리로 나누어 묻혀있다.
+// 2. 시추관을 수직으로 단 하나만 뚫을 수 있을 경우 가장 많은 석유를 뽑을 수 있는 위치를 찾으시오.
+
 function solution(land) {
-  const [n, m] = [land.length, land[0].length];
+  const [n, m] = [land.length, land[0].length]; // 지도의 크기를 구조 분해 할당을 통해 가져옴
 
-  // BFS 방문 처리를 위한 2차원 배열
+  // BFS 탐색을 위한 방문 처리 배열(2차원), 이동(상하좌우) 배열
   const visited = Array.from({ length: n }, () => new Array(m).fill(0));
-
-  // 상하좌우 이동
   const dx = [-1, 0, 1, 0];
   const dy = [0, 1, 0, -1];
 
-  const oilPerCol = new Array(m).fill(0); // 열별 석유량 누적 저장 배열
+  // 열 방향으로 이동 시 최대 석유량을 저장하는 1차원 배열(누적합)
+  const oilPerCol = new Array(m).fill(0);
 
-  // 가로로 이동하면서 시추관을 설치할 위치 찾기
   for (let x = 0; x < n; x++) {
-    // 시추관을 설치한 위치 아래로 끝까지 뻗어감
     for (let y = 0; y < m; y++) {
+      // 석유 매장지임과 동시에 방문하지 않은 위치일 경우
       if (land[x][y] && !visited[x][y]) {
         const queue = [[x, y]];
-        const cols = new Set([y]);
+        const cols = new Set([y]); // 해당 위치에서 방문할 수 있는 열 집합을 저장
 
-        visited[x][y] = 1;
         let size = 1;
+        visited[x][y] = 1;
 
-        // BFS 탐색 순회
+        // BFS 탐색
         while (queue.length) {
           const [currentX, currentY] = queue.shift();
 
+          // 상하좌우 이동
           for (let i = 0; i < 4; i++) {
             const [nextX, nextY] = [currentX + dx[i], currentY + dy[i]];
 
             if (nextX < 0 || nextY < 0 || nextX >= n || nextY >= m) continue;
             if (land[nextX][nextY] && !visited[nextX][nextY]) {
               visited[nextX][nextY] = 1;
-              size += 1;
-
-              cols.add(nextY);
               queue.push([nextX, nextY]);
+              size += 1;
+              cols.add(nextY);
             }
           }
         }
 
-        // 석유 덩어리가 포함된 모든 열에 양을 더함
+        // 석유 매장지 값 누적
         for (const col of cols) {
           oilPerCol[col] += size;
         }
@@ -47,5 +48,5 @@ function solution(land) {
     }
   }
 
-  return Math.max(...oilPerCol);
+  return Math.max(...oilPerCol); // 석유 매장지 중 최대 값을 반환
 }
